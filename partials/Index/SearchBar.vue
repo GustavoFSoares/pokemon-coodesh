8f42affd741234ca1b0b092cf7930d315a4e58fa
Gustavo Soares <gustavo.fe.soares@gmail.com>
2024-04-26 00:24:55 -0300

    feat: added search-bar

---
+++ partials/Index/SearchBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="search-bar">
+    <input
+      type="text"
+      v-model="input"
+      placeholder="Pesquise por nome ou código"
+    />
+
+    <button @click="handleClear">x</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const $emit = defineEmits(["update:modelValue"]);
+
+const input = ref<string | null>(null);
+
+function handleClear() {
+  input.value = null;
+}
+
+watchEffect(() => {
+  $emit("update:modelValue", input.value);
+});
+</script>
+
+<style lang="scss" scoped>
+.search-bar {
+  @apply h-12 bg-white w-full flex items-center pl-[37px] pr-[25px] rounded-full gap-4;
+
+  input {
+    @apply w-full flex-grow outline-none;
+  }
+}
+</style>
--- stores/pokemon.ts
+++ stores/pokemon.ts
@@ -6,15 +6,33 @@ export const usePokemonStore = defineStore({
   id: "pokemons",
   state: () => ({
     pokemons: [] as PokemonsList,
+    search: null as string | null,
   }),
   getters: {
     offset(state): number {
       return state.pokemons.length;
     },
+    filteredPokemons(state) {
+      if (!state.search) {
+        return state.pokemons;
+      }
+
+      return state.pokemons.filter((pokemon) => {
+        return (
+          pokemon.id === state.search ||
+          pokemon.name.includes(state.search || "")
+        );
+      });
+    },
   },
   actions: {
     sotrePokemons(pokemons: PokemonsList) {
       this.pokemons = [...this.pokemons, ...pokemons];
+    },
+    setSearch(input: string | null) {
+      console.log("1234", input);
+
+      this.search = input;
     },
   },
 });
