983f33dbfbb4c2169095be6f80459c0830eb4e62
Gustavo Soares <gustavo.fe.soares@gmail.com>
2024-04-25 21:25:47 -0300

    feat: styling pokemon list page

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,24 @@
 <template>
-  <h1>index page</h1>
+  <section class="pokemon-list-page">
+    <div class="pokemon-list-page__container">
+      <div class="pokemon-list-page__content">
+        <h1>index page</h1>
 
-  <PokemonsList />
+        <ClientOnly>
+          <ScrollLoader />
+        </ClientOnly>
+
+        <PokemonsList :data="data" />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts" setup>
 import { usePokemonStore } from "@/stores/pokemon";
 
 import PokemonsList from "@/partials/Index/PokemonsList.vue";
+import ScrollLoader from "@/partials/Index/ScrollWatcher.vue";
 
 const pokemonStore = usePokemonStore();
 
@@ -19,11 +30,47 @@ const { data } = await useFetch("/api/pokemon", {
   },
 });
 
-onMounted(() => {
+onServerPrefetch(async () => {
   if (!data.value) {
     return;
   }
 
-  pokemonStore.sotrePokemons(data.value);
+  await pokemonStore.sotrePokemons(data.value);
 });
+
+// onMounted(() => {
+//   if (!data.value) {
+//     return;
+//   }
+
+//   pokemonStore.sotrePokemons(data.value);
+// });
 </script>
+
+<style lang="scss" scoped>
+.pokemon-list-page {
+  @apply pr-9 pt-[18px] h-[100vh];
+
+  &__container {
+    @apply overflow-x-auto h-full;
+
+    &::-webkit-scrollbar {
+      @apply w-[13px] p-12;
+    }
+
+    /* Track */
+    &::-webkit-scrollbar-track {
+      @apply bg-[#C4C4C44F] rounded-[10px] overflow-hidden;
+    }
+
+    /* Handle */
+    &::-webkit-scrollbar-thumb {
+      @apply bg-[#C4C4C4] rounded-[10px] px-1 w-[10px];
+    }
+  }
+
+  &__content {
+    @apply max-w-[824px] mx-auto;
+  }
+}
+</style>
---
+++ partials/Index/ScrollWatcher.vue
@@ -0,0 +1,16 @@
+<template></template>
+
+<script setup>
+onMounted(() => {
+  document
+    .querySelector(".pokemon-list-page")
+    .addEventListener("scroll", () => {
+      console.log(
+        window.innerHeight + window.scrollY >= document.body.offsetHeight
+      );
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        console.log("FIM");
+      }
+    });
+});
+</script>
